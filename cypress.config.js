2b6ec02527ad7707e9cc0e506e3d537fdab41524
LuisAlbertoH <lahc30@gmail.com>
2025-08-07 10:54:11 -0600

    Create cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+	video: true,
+	pageLoadTimeout: 120000,
+	chromeWebSecurity: false,
+	experimentalSessionAndOrigin: true,
+	experimentalModifyObstructiveThirdPartyCode: true,
+  e2e: {
+    baseUrl: "https://opensource-demo.orangehrmlive.com",
+    supportFile: "cypress/support/e2e.js"
+  }, 
+  retries: {
+          runMode: 0, // Retries failed tests up to 2 times in run mode
+          openMode: 0, // Retries failed tests up to 1 time in open mode
+        }
+});
