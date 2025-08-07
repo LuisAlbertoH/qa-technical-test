cec0280e7879a427a365a9943e32ce180fdb12f1
LuisAlbertoH <lahc30@gmail.com>
2025-08-07 10:55:44 -0600

    Create commands.js

---
+++ cypress/support/commands.js
@@ -0,0 +1,6 @@
+Cypress.Commands.add('login', () => {
+  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  cy.get('[name="username"]').type('Admin');
+  cy.get('[name="password"]').type('admin123');
+  cy.get('button[type="submit"]').click();
+});
