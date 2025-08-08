259a512912d622b989ac6d4fa383c6ded30c6095
LuisAlbertoH <lahc30@gmail.com>
2025-08-08 13:34:06 -0600

    Update estrellablancachatbot.cy.js

--- cypress/e2e/estrellablancachatbot.cy.js
+++ cypress/e2e/estrellablancachatbot.cy.js
@@ -37,7 +37,7 @@ describe('Interact with Estrella Blanca Chatbot', () => {
     );
 
     //Interacting with the chatbot window
-    cy.get('iframe[src*="https://widget.botlers.io/window/index.html?v=219"]', { timeout: 20000 }).its('0.contentWindow.document').should('exist')
+    cy.get('iframe[src*="https://widget.botlers.io/window/index.html?v=220"]', { timeout: 20000 }).its('0.contentWindow.document').should('exist')
         .then((chatbotMessagesIframeDoc) => {
           console.log('Chatbot Messages contentWindow Document:', chatbotMessagesIframeDoc);
           
